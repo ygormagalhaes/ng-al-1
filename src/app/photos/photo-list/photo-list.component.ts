5d73818311b2e87b09fe1f3833daaf31fff4b9ee
Ygor Magalhaes <emaildoygor@gmail.com>
2019-07-24 16:31:07 -0300

    Unsubscribe no momento de destruição do componente photo-list para evitar problemas de memória

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { Photo } from '../photo/photo';
@@ -10,20 +10,24 @@ import { debounceTime } from 'rxjs/operators';
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.scss']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
   filter = '';
   photos: Photo[] = [];
   debounce: Subject<string> = new Subject<string>();
 
   constructor(
     private activatedRoute: ActivatedRoute
-  ) { }
+    ) { }
 
-  ngOnInit(): void {
-    this.photos = this.activatedRoute.snapshot.data.photos;
-    this.debounce
+    ngOnInit(): void {
+      this.photos = this.activatedRoute.snapshot.data.photos;
+      this.debounce
       .pipe(debounceTime(300))
       .subscribe(filter => this.filter = filter);
-  }
+    }
+
+    ngOnDestroy(): void {
+      this.debounce.unsubscribe();
+    }
 
 }
