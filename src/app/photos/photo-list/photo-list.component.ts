bbe3169786e8aff7e18b8c24790a7a7242e5376a
Ygor Magalhaes <emaildoygor@gmail.com>
2019-07-24 17:16:20 -0300

    Refactoring do código que carrega de forma páginada as fotos

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute } from '@angular/router';
 import { Photo } from '../photo/photo';
 import { Subject } from 'rxjs';
 import { debounceTime } from 'rxjs/operators';
+import { PhotoService } from '../photo/photo.service';
 
 @Component({
   selector: 'ap-photo-list',
@@ -14,12 +15,17 @@ export class PhotoListComponent implements OnInit, OnDestroy {
   filter = '';
   photos: Photo[] = [];
   debounce: Subject<string> = new Subject<string>();
+  hasMore = true;
+  currentPage = 1;
+  username: string;
 
   constructor(
-    private activatedRoute: ActivatedRoute
-    ) { }
+    private activatedRoute: ActivatedRoute,
+    private photoService: PhotoService
+  ) { }
 
     ngOnInit(): void {
+      this.username = this.activatedRoute.snapshot.params.username;
       this.photos = this.activatedRoute.snapshot.data.photos;
       this.debounce
       .pipe(debounceTime(300))
@@ -28,6 +34,25 @@ export class PhotoListComponent implements OnInit, OnDestroy {
 
     ngOnDestroy(): void {
       this.debounce.unsubscribe();
+    }
+
+    load() {
+      this.photoService.listPhotosFromUserPaginated(this.username, ++this.currentPage)
+        .subscribe(photos => this.setHasMoreValue(photos));
+        // .subscribe(photos => this.setHasMoreValue(photos)); FIXME: Problema com 'this' com esse uso.
+    }
+
+    private setHasMoreValue(photos: Photo[]): void {
+      if (photos.length) {
+        this.photos = this.photos.concat(photos);
+        if (photos.length < 3) {
+          this.hasMore = false;
+        } else {
+          this.hasMore = true;
+        }
+      } else {
+        this.hasMore = false;
+      }
     }
 
 }
