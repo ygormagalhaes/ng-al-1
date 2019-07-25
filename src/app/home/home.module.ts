2e32a6f4df03abae6390fda926dae18a944d2718
Ygor Magalhaes <emaildoygor@gmail.com>
2019-07-25 14:47:19 -0300

    Criação de componente para exibição de mensagens de erros de formulários

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
-import { SiginComponent } from './signin/signin.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+
+import { SiginComponent } from './signin/signin.component';
+import { ValidationMessageModule } from '../shared/components/validation-message/validation-message.module';
 
 @NgModule({
     declarations: [ SiginComponent ],
     imports: [
         CommonModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        ValidationMessageModule
     ]
 })
 export class HomeModule { }
---
+++ src/app/shared/components/validation-message/validation-message.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'ap-validation-message',
+    templateUrl: './validation-message.component.html'
+})
+export class ValidationMessageComponent {
+    @Input()
+    message = '';
+}
