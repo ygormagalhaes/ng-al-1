d1b516a2c79c180bb835bd29d1fe615664c1aee6
Ygor Magalhaes <emaildoygor@gmail.com>
2019-07-25 16:35:57 -0300

    Implementação através do operator tap de código responsável pela extração do token da resposta após login

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root'
@@ -11,6 +12,13 @@ export class AuthService {
     constructor(private httpClient: HttpClient) { }
 
     login(credentials: {userName: string, password: string}): Observable<any> {
-        return this.httpClient.post(`${this.LOGIN_API}user/login`, credentials);
+        return this.httpClient.post(
+            `${this.LOGIN_API}user/login`,
+            credentials,
+            { observe: 'response' })
+            .pipe(tap((res: HttpResponse<any>) => {
+                const authToken = res.headers.get('x-access-token');
+                console.log(`User ${credentials.userName} logged with token ${authToken}`);
+            }));
     }
 }
