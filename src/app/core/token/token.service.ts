f2d9df66a85f8b1655567f1fa4b97d4da9153c72
Ygor Magalhaes <emaildoygor@gmail.com>
2019-07-25 16:45:24 -0300

    Implementação de token service

---
+++ src/app/core/token/token.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class TokenService {
+    private readonly TOKEN_KEY = 'authToken';
+
+    hasToken(): boolean {
+        return !!this.getToken();
+    }
+
+    setToken(token: string): void {
+        window.localStorage.setItem(this.TOKEN_KEY, token);
+    }
+
+    getToken(): string {
+        return window.localStorage.getItem(this.TOKEN_KEY);
+    }
+
+    removeToken(): void {
+        window.localStorage.removeItem(this.TOKEN_KEY);
+    }
+}
