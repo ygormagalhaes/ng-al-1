968fc11d02ec1f9797265ab437df6b9ae9ea4687
Ygor Magalhaes <emaildoygor@gmail.com>
2019-07-25 16:47:31 -0300

    Utilização do TokenService por AuthService para salvar o token em localstorage

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
+import { TokenService } from '../token/token.service';
 
 @Injectable({
     providedIn: 'root'
@@ -9,7 +10,10 @@ import { tap } from 'rxjs/operators';
 export class AuthService {
     private readonly LOGIN_API = 'http://localhost:3000/';
 
-    constructor(private httpClient: HttpClient) { }
+    constructor(
+        private httpClient: HttpClient,
+        private tokenService: TokenService
+    ) { }
 
     login(credentials: {userName: string, password: string}): Observable<any> {
         return this.httpClient.post(
@@ -18,6 +22,7 @@ export class AuthService {
             { observe: 'response' })
             .pipe(tap((res: HttpResponse<any>) => {
                 const authToken = res.headers.get('x-access-token');
+                this.tokenService.setToken(authToken);
                 console.log(`User ${credentials.userName} logged with token ${authToken}`);
             }));
     }
