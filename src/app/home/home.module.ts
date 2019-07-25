82e4457041bd47d25b23de3389b661c2cc0b1cb6
Ygor Magalhaes <emaildoygor@gmail.com>
2019-07-25 14:33:37 -0300

    Validação inicial do formulário com módulo ReactiveFormsModule

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { SiginComponent } from './signin/signin.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-    declarations: [ SiginComponent ]
+    declarations: [ SiginComponent ],
+    imports: [
+        CommonModule,
+        ReactiveFormsModule
+    ]
 })
 export class HomeModule { }
