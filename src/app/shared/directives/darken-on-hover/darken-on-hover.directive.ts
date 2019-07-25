87569751ac7642238f10dd8193160cc10730b606
Ygor Magalhaes <emaildoygor@gmail.com>
2019-07-25 13:35:35 -0300

    Implementação de inbound property para definição do brilho desejado a ser utilizado com a diretiva

--- src/app/shared/directives/darken-on-hover/darken-on-hover.directive.ts
+++ src/app/shared/directives/darken-on-hover/darken-on-hover.directive.ts
@@ -1,15 +1,18 @@
-import { Directive, HostListener, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, HostListener, ElementRef, Renderer2, Input } from '@angular/core';
 
 @Directive({
     selector: '[darkenOnHover]' // [] para directive ser utilizada como property
 })
 export class DarkenOnHoverDirective {
 
+    @Input()
+    brightness = '70%';
+
     constructor(private el: ElementRef, private renderer: Renderer2) { }
 
     @HostListener('mouseenter')
     darkenOn(): void {
-        this.renderer.setStyle(this.el.nativeElement, 'filter', 'brightness(70%)');
+        this.renderer.setStyle(this.el.nativeElement, 'filter', `brightness(${this.brightness})`);
     }
 
     @HostListener('mouseleave')
