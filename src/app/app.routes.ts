ee7ae15d7d2a541dc06f05efbce07c3bdfb68e15
Ygor Magalhaes <emaildoygor@gmail.com>
2019-07-26 15:10:55 -0300

    Criação inicial do componente para cadastro de novos usuários

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AuthGuard } from './core/auth/auth.guard';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { SiginComponent } from './home/signin/signin.component';
+import { SigupComponent } from './home/signup/signup.component';
 import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
-import { NotFoundComponent } from './errors/not-found/not-found.component';
-import { SiginComponent } from './home/signin/signin.component';
-import { AuthGuard } from './core/auth/auth.guard';
 
 const routes: Routes = [
-    { path: '', component: SiginComponent, canActivate: [ AuthGuard ] },
+    { path: '', component: SiginComponent, canActivate: [AuthGuard] },
+    { path: 'signup', component: SigupComponent },
     {
         path: 'user/:username', component: PhotoListComponent, resolve: {
             photos: PhotoListResolver
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { SiginComponent } from './signin/signin.component';
 import { ValidationMessageModule } from '../shared/components/validation-message/validation-message.module';
+import { SigupComponent } from './signup/signup.component';
 
 @NgModule({
-    declarations: [ SiginComponent ],
+    declarations: [
+        SiginComponent,
+        SigupComponent
+    ],
     imports: [
         CommonModule,
+        FormsModule,
         ReactiveFormsModule,
         ValidationMessageModule,
         RouterModule
