25c10fd6872e4bf116f4e8c6bf50e355cfb63d15
Ygor Magalhaes <emaildoygor@gmail.com>
2019-07-29 16:48:07 -0300

    Implementação através de getter e setter para correta exibição de imagems em base64 e formatos convencionais

--- src/app/photos/photo/photo.component.ts
+++ src/app/photos/photo/photo.component.ts
@@ -5,9 +5,22 @@ import { Component, Input } from '@angular/core';
     templateUrl: './photo.component.html'
 })
 export class PhotoComponent {
+    private readonly API_URL = 'http://localhost:3000/';
+    realUrl: string;
+
     @Input()
     description: string;
 
     @Input()
-    url: string;
+    set url(urlParam: string) {
+        if (!urlParam.startsWith('data')) {
+            this.realUrl = `${this.API_URL}imgs/${urlParam}`;
+        } else {
+            this.realUrl = urlParam;
+        }
+    }
+
+    get url(): string {
+        return this.realUrl;
+    }
 }
