41374af21a63cbdaff29240b0cf2b7c957374de9
Ygor Magalhaes <emaildoygor@gmail.com>
2019-07-29 11:58:53 -0300

    Criação de componente home com imagem lateral e utilização de rotas filhas para cadastro e login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,10 +8,17 @@ import { SignupComponent } from './home/signup/signup.component';
 import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-    { path: '', component: SiginComponent, canActivate: [AuthGuard] },
-    { path: 'signup', component: SignupComponent },
+    {
+        path: '',
+        component: HomeComponent,
+        children: [
+            { path: '', component: SiginComponent, canActivate: [AuthGuard] },
+            { path: 'signup', component: SignupComponent },
+        ]
+    },
     {
         path: 'user/:username', component: PhotoListComponent, resolve: {
             photos: PhotoListResolver
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -7,9 +7,11 @@ import { SiginComponent } from './signin/signin.component';
 import { ValidationMessageModule } from '../shared/components/validation-message/validation-message.module';
 import { SignupComponent } from './signup/signup.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home.component';
 
 @NgModule({
     declarations: [
+        HomeComponent,
         SiginComponent,
         SignupComponent
     ],
