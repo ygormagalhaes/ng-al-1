7d9917052fa762f6645564070c8096fb6ef51ec0
Ygor Magalhaes <emaildoygor@gmail.com>
2019-08-01 16:51:10 -0300

    WIP

--- src/app/errors/global-error-handler/global-error-handler.ts
+++ src/app/errors/global-error-handler/global-error-handler.ts
@@ -3,7 +3,7 @@ import { ErrorHandler } from '@angular/core';
 export class GlobalErrorHandler implements ErrorHandler {
 
     handleError(error: any): void {
-        console.log('Implementar meu global error handler.')
+        console.log('Implementar meu global error handler.');
         throw error;
     }
 }
