1abb6bfa3ada314aa7312498ffdf0f4bb85b6ba3
Ygor Magalhaes <emaildoygor@gmail.com>
2019-08-01 16:49:14 -0300

    Implementação inicial de um global error handler

--- src/app/errors/errors.module.ts
+++ src/app/errors/errors.module.ts
@@ -1,13 +1,20 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { PhotosModule } from '../photos/photos.module';
+import { GlobalErrorHandler } from './global-error-handler/global-error-handler';
 
 @NgModule({
   declarations: [NotFoundComponent],
   imports: [
     CommonModule,
     PhotosModule
+  ],
+  providers: [
+    {
+      provide: ErrorHandler,
+      useClass: GlobalErrorHandler
+    }
   ]
 })
 export class ErrorsModule { }
