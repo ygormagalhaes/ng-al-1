e72a557271dc6695529504598a3dbde0cd8218ee
Ygor Magalhaes <emaildoygor@gmail.com>
2019-08-01 17:30:54 -0300

    Montagem de objeto que representa erro para ser enviado a um serviço do back end

--- src/app/errors/global-error-handler/global-error-handler.ts
+++ src/app/errors/global-error-handler/global-error-handler.ts
@@ -1,14 +1,38 @@
-import { ErrorHandler } from '@angular/core';
+import { ErrorHandler, Injector } from '@angular/core';
 import * as StackTrace from 'stacktrace-js';
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { UserService } from 'src/app/core/user/user.service';
 
 export class GlobalErrorHandler implements ErrorHandler {
 
+    constructor(private injector: Injector) { }
+
     handleError(error: any): void {
+        const location = this.injector.get(LocationStrategy);
+        const userService = this.injector.get(UserService);
+
+        const url = location instanceof PathLocationStrategy
+            ? location.path : '';
+
+        const message = error instanceof Error
+            ? error.message : error.toString();
+
         StackTrace.fromError(error).then(errorArray => {
-            const error2String = errorArray
+            const stringStack = errorArray
                 .map(frame => frame.toString())
                 .join('\n');
-            console.log(error2String);
+
+            const errorObject = {
+                username: userService.getUsername(),
+                url,
+                message,
+                stack: stringStack
+            };
+            console.log(message);
+            console.log(stringStack);
+
+            console.log('Erro a ser enviado:');
+            console.log(errorObject);
         });
     }
 }
