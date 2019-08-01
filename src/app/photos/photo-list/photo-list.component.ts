5beaae612863793ad3d9ae01a1a02daa6819bf7d
Ygor Magalhaes <emaildoygor@gmail.com>
2019-08-01 14:45:24 -0300

    Implementações do componente de exibição de estado de trabalho para requisições ajax

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 
 import { Photo } from '../photo/photo';
 import { PhotoService } from '../photo/photo.service';
+import { LoadingService } from 'src/app/shared/components/loading/loading.service';
 
 @Component({
   selector: 'ap-photo-list',
@@ -18,7 +19,8 @@ export class PhotoListComponent implements OnInit {
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    private photoService: PhotoService
+    private photoService: PhotoService,
+    private loadingService: LoadingService
   ) { }
 
     ngOnInit(): void {
@@ -26,6 +28,7 @@ export class PhotoListComponent implements OnInit {
         this.username = this.activatedRoute.snapshot.params.username;
         this.photos = this.activatedRoute.snapshot.data.photos;
       });
+      this.loadingService.start();
     }
 
     load() {
